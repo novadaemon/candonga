da22d351cc10721adbfdf198c7771371a6d2109c
novadaemon <novadaemon@gmail.com>
2021-06-30 14:43:43 -0400

    Add base url tag to layout view

--- novadaemon/candonga/src/Candonga/views/layout.blade.php
+++ novadaemon/candonga/src/Candonga/views/layout.blade.php
@@ -2,12 +2,13 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <base href="{{url('.')}}" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Candonga Shopstore</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="{{ secure_asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <meta name="theme-color" content="#563d7c">
     <style>
@@ -57,7 +58,7 @@
     </footer>
 </div>
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script src="{{ secure_asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     @stack('js')
 </body>
 </html>
